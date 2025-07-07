a2744526c638d5c6eb0a3bd2c3ceafa0d8123af4
Thejaswini Velvaluri <thejuvelvaluri@gmail.com>
2025-07-07 14:44:22 +0530

    Fix: wildcard route for Render with Node 22

--- server/app.js
+++ server/app.js
@@ -29,8 +29,8 @@ mongoose.connect(process.env.MONGO_URI)
 // ✅ Serve frontend (React build folder)
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 // ✅ Start the server
